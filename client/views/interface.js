d058ec2ce89a28c4ee48bf6196378731bd9d18fc
Sangeet Gyawali <sangeight@gmail.com>
2014-07-02 17:51:51 +0100

    Add image into the stage. Need to bind the resizable and draggable handlers

--- client/views/interface.js
+++ client/views/interface.js
@@ -1,6 +1,37 @@
 Template.stage.rendered = function () {
+	$("#stage div").draggable({ stack: '#stage div' });
 	$(".resizable").resizable();
-	$(".draggable").draggable({ stack: '.draggable'});
-
-
 };
+
+Template.entities.events({
+	'submit form[name=media]': function (e, tpl) {
+		e.preventDefault();
+		var imageUrl = $(e.target).find('input[name=imageUrl]').val();
+
+		// Further optimisation can be done by using javascript instead of jquery
+		$('#stage').append(
+			$('<div>', {
+				class: 'draggable',
+				html: $('<img>', {class:'resizable', src: imageUrl})
+			})
+		);
+
+
+		var media = {
+			currentCrisisId: this._id,
+			mrMediaUrl: imageUrl,
+			dctermsFormat: 'image/jpeg' // Mime type
+		}
+
+		// Meteor.call('newMedia', media, function(error, id) {
+	 //    if (error)
+  //       return alert(error.reason);    
+	 //  });
+
+	console.log("media", media);
+
+
+
+
+	}
+});
