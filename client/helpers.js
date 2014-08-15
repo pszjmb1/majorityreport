f15df0a4345b891d56f6f3c77d41097d3c496b14
Sangeet Gyawali <sangeight@gmail.com>
2014-08-15 17:43:56 +0100

    Uncertainity model - adding multiple entity attributes, agree to certain value, assign certainity confidence

---
+++ client/helpers.js
@@ -0,0 +1,45 @@
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+UI.registerHelper('printObject', function(obj) {
+    return JSON.stringify(obj);
+});
+
+UI.registerHelper('prettyDate', function(date) {
+    if(moment(date).isValid()) {
+        return moment(date).format(dateWithTimeFormat);
+    }
+});
+
+// Agents and Users
+UI.registerHelper('isOwner', function(agent) {
+	if(agent) {
+      return agent.mrUserId == Meteor.userId();
+    }
+});
+
+
+UI.registerHelper('getAgentName', getAgentName);
+
+UI.registerHelper('getAgentNameById', function(agentId) {
+	var agent = Provenance.findOne({mrUserId: agentId}),
+		name = getAgentName(agent);
+
+	if(name) { return name; }
+});
+
+/**
+ * Shared Helpers for UI.helpers
+ */
+function getAgentName(agent) {
+	if(agent) {
+		if( agent.foafGivenName){
+		  return agent.foafGivenName +" "+  agent.foafFamilyName;
+		}else{
+		  return agent.mrUserName;
+		}
+	}
+}
+
+
