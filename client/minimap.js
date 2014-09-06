2189c0aec52afc769f3ebe806a577e02b861124a
Sangeet Gyawali <sangeight@gmail.com>
2014-09-06 16:18:34 +0100

    Minimap - WIP

--- client/minimap.js
+++ client/minimap.js
@@ -20,7 +20,7 @@ function MiniMap() {
 
 	this.filterNodesBy = {
 		tags: ['div'],
-		classes: ['entity-outer'],
+		classes: ['entity-outer', 'entity-inner'],
 		attributes: ['style']
 	};
 
@@ -62,72 +62,67 @@ MiniMap.prototype.drawMap = function() {
 				(mapWrapperRect.height / containerRect.height)
 			);
 
-
-	var styles = {
-		width: 					containerRect.width +'px',
-		height: 				containerRect.height + 'px',
-		position:               'absolute',
-      	top:                    0,
-      	left:                   0,
-		'transform': 			{value: 'scale('+scale+')', autoPrefix: true},
-		'transform-origin': 	{value: 'top left', autoPrefix: true},
-	}
-
 	if(self.mapBox !== null) { self.mapWrapperInner.removeChild(self.mapBox); }
 
 	self.mapBox = self.container.cloneNode(true);
-	self.mapBox.style.cssText += _generateCssText(styles);
+	var processed = _processNodes(self.mapBox, self.filterNodesBy),
+		mapPostion = {};
 
-	_cleanNodes(self.mapBox.childNodes, self.filterNodesBy);
+	mapPostion.top = (processed.minimum.top < 0) ? Math.abs(processed.minimum.top) * scale : 0;
+	mapPostion.left = (processed.minimum.left < 0) ? Math.abs(processed.minimum.left) * scale: 0;
+
+	self.mapbox = processed.nodes;
+
+	self.mapBox.style.cssText = '';
+	$(self.mapBox).css({
+		width: 					containerRect.width +'px',
+		position:               'absolute',
+      	top:                    mapPostion.top,
+      	left:                   mapPostion.left,
+		'transform': 			'scale('+scale+')',
+		'transform-origin': 	'top left',
+	});
 	self.mapWrapperInner.appendChild(self.mapBox);
 };
 
-var _generateCssText = function(obj) {
-	if(obj !== null && typeof obj === 'object') {
-		var output = '';
-		for( var prop in obj) {
-			if(typeof obj[prop] === 'object' && obj[prop].hasOwnProperty('autoPrefix')) {
-				var style = prop +':'+ obj[prop].value +';';
-				output += style;
-				output += '-webkit-'+ style;
-				output += '-ms-'+ style;
-				output += '-moz-'+ style;
-			} else {
-				output += prop +':'+ obj[prop] +';';
-			}
-		}
-
-		return output;
-	}
-}
-
 //** returns the minimum top and left positions (useful when -ve values)
-var _cleanNodes = function(nodes, filters) {
-	if(!nodes || nodes.length < 1) return;
+var _processNodes = function(nodes, filters) {
+	if(!nodes.childNodes || nodes.childNodes.length < 1) return;
 
 	filters.tags = filters.tags || [];
 	filters.classes = filters.classes || [];
 	filters.attributes = filters.attributes || [];
 
-	for (var i=0; i < nodes.length; i++) {
-		if(nodes[i].hasOwnProperty('tagName')) {
-			
-			if(nodes[i].childNodes.length > 0) {
-				_cleanNodes(nodes[i].childNodes, filters);
-			}
-			// Array.indexOf only supoorted in IE9+
-			if((filters.tags.length > 0 && filters.tags.indexOf(nodes[i].tagName) > -1)
-				|| 
-				(filters.classes.length > 0 && checkClasses(filters.classes, nodes[i].className) )) {
+	var filterSelectors = '', minimum = {top: 0, left: 0};
 
-				console.log(checkClasses(filters.classes, nodes[i].className), nodes[i].className );
+	// Prepare the selectors based on value from 
+	if(filters.tags.length > 0) { filterSelectors += filters.tags.join(','); }
+	if(filters.classes.length > 0) { filterSelectors += ',.'+ filters.classes.join(',.')}
+
+	if(filters.attributes.length > 0) {
+		$(nodes).find(filterSelectors).each(function(){
+		  	var attributes = this.attributes,
+		  		i = attributes.length;
+
+		  	// Remove the attributes using the whitelist
+			while( i-- ){
+				if(!$.inArray(attributes[i], filters.attributes)) {
+				  	this.removeAttributeNode(attributes[i]);
+				}
 			}
 			
-		} else {
-			delete nodes[i];
-		}
-	};
+			if(this.style.top) { 
+				var topValue = parseFloat(this.style.top);
+				minimum.top = (topValue < minimum.top ) ? topValue : minimum.top;
+			}
+			if(this.style.left) { 
+				var leftValue = parseFloat(this.style.left);
+				minimum.left = (leftValue < minimum.left ) ? leftValue : minimum.left;
+			}
 
-	return nodes;
+		});
+	}
+
+	return { nodes: nodes, minimum: minimum};
 }
 
